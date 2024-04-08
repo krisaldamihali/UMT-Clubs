af9f946d4fadcb09ca8896241ebf29c6daf22950
Krisalda Mihali <krisaldamihali02@gmail.com>
2024-04-08 20:05:12 +0200

    umt

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,8 +7,6 @@ import Clubs from '../Components/Clubs.js';
 import WebDev from '../Components/WebDev.js';
 import GDSC from '../Components/GDSC.js';
 import AppDev from '../Components/AppDev.js';
-import GameDev from '../Components/GameDev.js';
-import AI from '../Components/AI.js';
 import CyberSec from '../Components/CyberSec.js';
 import Navbar from './Navbar.js';
 
@@ -25,8 +23,6 @@ function App() {
           <Route path="/web-development" element={<WebDev />} /> 
           <Route path="/gdsc" element={<GDSC />} /> 
           <Route path="/app-development" element={<AppDev />} /> 
-          <Route path="/game-development" element={<GameDev />} /> 
-          <Route path="/ai" element={<AI />} />
           <Route path="/cybersecurity" element={<CyberSec />} />
         </Routes>
       </div>
--- src/Components/Clubs.js
+++ src/Components/Clubs.js
@@ -25,7 +25,7 @@ const categories = [
     title: "Game Development",
     image: catg3Image,
     info: "Provides resources and guidance for developing digital games.",
-    link: "/game-development"
+    link: "https://clubs.umt.edu.al/game/index.html"
   },
   {
     title: "Cybersecurity",
@@ -37,7 +37,7 @@ const categories = [
     title: "AI",
     image: catg4Image,
     info: "A source for artificial intelligence science and AI application development.",
-    link: "/ai"
+    link: "https://aidevclub.wixsite.com/ai-developers-club"
   },
   {
     title: "GDSC",
